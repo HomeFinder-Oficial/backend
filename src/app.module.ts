a40fbb0499b10fcd523dc596e2c46a79236f68b2
Juan Hoyos <juanhoyos1347@gmail.com>
2025-10-20 15:59:49 -0500

    Fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
-import { InfraestructureModule } from './infraestructure/infraestructure.module';
+import { InfraestructureModule } from './infrastructure/infraestructure.module';
 
 @Module({
   imports: [CoreModule, InfraestructureModule],
