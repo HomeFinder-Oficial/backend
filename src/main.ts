592af5171b5c7c701a4800492e3cb83427597ed8
Juan Hoyos <juanhoyos1347@gmail.com>
2025-10-25 23:55:13 -0500

    Feat: add winston

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(InfrastructureModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
